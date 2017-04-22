755d1853cf79e07413cf63b7f69887509974455e
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2017-04-21 22:06:01 -0400

    Convert coordinates to zip code

--- app/src/main/java/whitaker/anthony/bootcamplocator/fragment/MainFragment.java
+++ app/src/main/java/whitaker/anthony/bootcamplocator/fragment/MainFragment.java
@@ -1,13 +1,21 @@
 package whitaker.anthony.bootcamplocator.fragment;
 
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,8 +25,10 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import whitaker.anthony.bootcamplocator.R;
 import whitaker.anthony.bootcamplocator.activity.MapsActivity;
@@ -33,6 +43,9 @@ import whitaker.anthony.bootcamplocator.service.DataService;
 public class MainFragment extends Fragment implements OnMapReadyCallback {
 
     public static final String LOG_TAG = MainFragment.class.getSimpleName();
+
+    /** Standard zip code length for user location. Use constant for Development only. */
+    public static final int ZIP_CODE_LENGTH = 5;
 
 
     private GoogleMap map;
@@ -67,14 +80,55 @@ public class MainFragment extends Fragment implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        final EditText zipCodeText = (EditText)view.findViewById(R.id.zipCodeText);
+        zipCodeText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(KeyEvent.ACTION_DOWN == event.getAction() && KeyEvent.KEYCODE_ENTER == keyCode) {
+                    String text = zipCodeText.getText().toString();
+
+                    int zipCode;
+                    try {
+                        zipCode = parseZipCode(text);
+                    } catch (IllegalArgumentException e) {
+                        Toast.makeText(getContext(), "Invalid Zip Code", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+
+                    dismissKeyboard(zipCodeText.getWindowToken());
+
+                    updateMapForZipCode(zipCode);
+                    return true;
+                }
+                return false;
+            }
+        });
+
         return view;
+    }
+
+    private void dismissKeyboard(IBinder windowToken) {
+        InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(windowToken, 0);
+    }
+
+    private int parseZipCode(String text) throws IllegalArgumentException {
+        String trimmedText = text.trim();
+        if(trimmedText.length() != ZIP_CODE_LENGTH) {
+            throw new IllegalArgumentException("Invalid zip code: " + text);
+        }
+
+        try {
+            return Integer.parseInt(trimmedText);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid zip code: " + text);
+        }
     }
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -89,6 +143,18 @@ public class MainFragment extends Fragment implements OnMapReadyCallback {
             userMarker = new MarkerOptions().position(latLng).title("Current Location");
             map.addMarker(userMarker);
             Log.v(LOG_TAG, "Current location: " + latLng);
+        }
+
+        Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            int zipCode = parseZipCode(addresses.get(0).getPostalCode());
+            updateMapForZipCode(zipCode);
+        } catch (IOException e) {
+            Log.d(LOG_TAG, e.getMessage(), e);
+        } catch (IllegalArgumentException e) {
+            Toast.makeText(getContext(), "Invalid Zip Code", Toast.LENGTH_SHORT).show();
+            Log.e(LOG_TAG, "Faulty zip code.", e);
         }
 
         updateMapForZipCode(92284);
